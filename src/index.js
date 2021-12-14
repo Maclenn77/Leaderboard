0323323662dea0ce9775766ec4f32dd99660a44e
Maclenn77 <maclenn77@gmail.com>
2021-12-14 12:40:55 -0600

    Solve linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 const leaderBoard = document.querySelector('.leaderboard');
 
-leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
-leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
-leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
-leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
+leaderBoard.insertAdjacentHTML('beforeend', '<li>Name: Score </li> ');
+leaderBoard.insertAdjacentHTML('beforeend', '<li>Name: Score </li> ');
+leaderBoard.insertAdjacentHTML('beforeend', '<li>Name: Score </li> ');
+leaderBoard.insertAdjacentHTML('beforeend', '<li>Name: Score </li> ');
