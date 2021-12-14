d9ef46558e948b6cd294dacdf73a00735fe9979c
Maclenn77 <maclenn77@gmail.com>
2021-12-14 12:39:16 -0600

    Solve linters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import _ from 'lodash';
 import './style.css';
 
 const leaderBoard = document.querySelector('.leaderboard');
 
-leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
-leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
-leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
-leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
+leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
+leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
+leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
+leaderBoard.insertAdjacentHTML('beforeend', `<li>Name: Score </li> `);
