7d31540a7232194946545ac00d446750396aaa56
Maclenn77 <maclenn77@gmail.com>
2021-12-14 12:31:37 -0600

    Solve linters

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import './style.css';
 
 const leaderBoard = document.querySelector('.leaderboard');
 
-
-function component() {
-    const score = document.createElement('li')
-  
-   // Lodash, now imported by this script
-    score.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    score.classList.add('hello');
-  
-    return score;
-  }
-  
-
 leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
 leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
 leaderBoard.insertAdjacentHTML('beforeend', `<li class='test border border-dark'>Name: Score </li> `)
