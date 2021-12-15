29b8edf77d4d1171a1de376774f99dc1b328f17c
Maclenn77 <maclenn77@gmail.com>
2021-12-15 09:11:17 -0600

    Add url and create function for fetch api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,23 @@
-export function refresh(element) {
-    element.insertAdjacentHTML('beforeend', '<li>Name: Score </li> ');
+const gameId = 'RAxHi8yBkg2BqWCgax3x';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/' + gameId + '/scores/';
+
+export async function refresh(element) {
+  const response = await fetch(url);
+  const json = await response.json();
+  alert(json.result[0].user)
 }
 
 export function submit(name, score) {
-    alert(name + ': ' + score);
+  fetch(url, {
+   method: 'POST',
+   body: JSON.stringify({
+     user: name,
+     score: score,
+   }),
+   headers: {
+     'Content-type': 'application/json; charset=UTF-8',
+   },
+  })
+    .then((response) => response.json())
+    .then((json) => alert(json.result[1].user));
 }
